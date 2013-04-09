5432c03a8b28e2e8ca1c7fe586c40a4ebaa729a3
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-04-09 12:22:38 +0200

    Update for 4.0.4

--- zotero_installer.sh
+++ zotero_installer.sh
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with qnotero.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION="3.0.13"
+VERSION="4.0.4"
 if [ `uname -m` == "x86_64" ]; then
 	ARCH="x86_64"
 else
